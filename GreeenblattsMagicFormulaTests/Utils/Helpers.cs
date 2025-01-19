45e9bc8101ae9434fdf3083cece0eb2e80174b8d
Roberts Kovalauskis <roberts.kovalauskis@globalrelay.net>
2025-01-19 08:26:02 -0800

    Added a Test Helper method

---
+++ GreeenblattsMagicFormulaTests/Utils/Helpers.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+
+namespace GreeenblattsMagicFormulaTests.Utils
+{
+    public static class Helpers
+    {
+        public static async Task<JsonDocument> ConvertResponseToJsonDocument(HttpResponseMessage response)
+        {
+            Assert.AreEqual(200, (int)response.StatusCode, "Expected HTTP status code 200.");
+            var incomeStatementContent = await response.Content.ReadAsStringAsync();
+            return JsonDocument.Parse(incomeStatementContent);
+        }
+    }
+}
